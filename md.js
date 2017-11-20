bbcb0dc7299bc19407eb8515083098e3dfaf58c4
woshichenyaofei <13935863746@163.com>
2017-11-20 21:14:11 +0800

    update

--- md.js
+++ md.js
@@ -19,10 +19,8 @@ function translate(filename){
     // 使用 MarkdownJS 模块把源文件转换为 HTML 源代码
     // fileContent = markdownv.toHTML(fileContent);
     fileContent = marked(fileContent);
-    
     // 保存
-    const start="<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Document</title></head><link rel='stylesheet' href='style.css'><link rel='stylesheet' href='md.css'><body><div class='toplength'><div class='top'><a href='/index' class='topleft'>陈耀辉</a><a href='/views/about' class='topright'>About</a></div></div><div class='contentbox'><div class='content'>";
-    const end="<div class='pagination'><a href='' class='pre'>&ltPre</a><a href='' class='next'>Next&gt</a></div></div></div><div class='footer'>@2012-2017</div></body></html>";
-    fs.writeFileSync('./views/'+filename+'.html', start+fileContent+end);
+    const start="<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Document</title></head><link rel='stylesheet' href='../css/style.css'><link rel='stylesheet' href='../css/md.css'><body><div class='toplength'><div class='top'><a href='/index' class='topleft'>陈耀辉</a><a href='/views/about' class='topright'>About</a></div></div><div class='contentbox'><div class='content'>";
+    fs.writeFileSync('./views/'+filename+'-page.html', start+fileContent);
 }
 // 读入 Markdown 源文件
