ff56aa1370215ee5d80a4b644869953beb8506db
woshichenyaofei <13935863746@163.com>
2017-11-29 17:49:07 +0800

    update

--- md.js
+++ md.js
@@ -23,8 +23,9 @@ function translate(filename) {
         breaks: true
     });
     // 保存
-    const start = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Document</title></head><link rel='stylesheet' href='../css/md.css'><link rel='stylesheet' href='../css/style.css'><body><div class='toplength'><div class='top'><a href='../index.html' class='topleft'>陈耀辉</a><a href='about.html' class='topright'>About</a></div></div><div class='contentbox'><div class='content'>";
+    const start = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Document</title></head><link rel='stylesheet' href='../css/md.css'><link rel='stylesheet' href='../css/style.css'><body><div class='toplength'><div class='top'><a href='../index.html' class='topleft'>陈耀辉</a><a href='about.html' class='topright'>About</a></div></div><div class='contentbox'><div class='content'><div class='contentInner'>";
+    const end="</div></div><div class=footer>@2016-2017</div></html>";
     const address = filename === 'about' ? ('./views/' + filename + '.html') : ('./views/' + filename + '-page.html');
-    fs.writeFileSync(address, start + fileContent);
+    fs.writeFileSync(address, start + fileContent + end);
 }
 // 读入 Markdown 源文件
